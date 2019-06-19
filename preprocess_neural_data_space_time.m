f8459300c826994b767314fdd2329ae7e8f31c46
juangallego <gallego.juanalvaro@gmail.com>
2019-06-19 11:12:39 +0200

    more stuff

---
+++ preprocess_neural_data_space_time.m
@@ -0,0 +1,32 @@
+%
+% Preprocess data (downsamples and de-means) and computes distances
+%
+
+
+
+% ---------------------------------------------------------------------
+% Retrieve and compute basic stuff
+
+% Cells' coordinates
+xc = [data.stat.xglobal];
+yc = [data.stat.xglobal];
+
+% get number of neurons
+nc = numel(xc);
+
+% distance between somata
+dist = sqrt( (repmat(xc,nc,1) - repmat(xc',1,nc)).^2 + ...
+    (repmat(yc,nc,1) - repmat(yc',1,nc)).^2 );
+
+
+% ---------------------------------------------------------------------
+% Pre-processing
+
+% downsample
+X = bin2d( data.S(:,1:end), par.tbin, 2 );
+
+% de-mean
+X = X - mean(X,2);
+
+% get number of bins
+nt = size(X,2);
